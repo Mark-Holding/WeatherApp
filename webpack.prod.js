271b6fc3ce8a145089847b7d6abb55c277ffa683
Mark H <mark-holding@hotmail.co.uk>
2025-01-14 20:29:11 +0000

    Initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,11 @@
+webpack.prod.js
+
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  // Add production-specific optimizations here if needed
+});
+
+
